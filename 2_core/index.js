ec0f4fa12694eb8517d1c504dc6306806ebd1d74
unknown <cristianoans@hotmail.com>
2023-04-10 13:19:24 -0300

    atividade de modules core

--- 2_core/index.js
+++ 2_core/index.js
@@ -1,5 +1,12 @@
-
 const os = require("os");
 const fs = require("fs");
 
-console.log(os.userInfo.toString)
+const arquitetura = os.arch();
+const plataforma = os.platform();
+const usuario = os.hostname();
+const versao = os.version();
+
+fs.writeFileSync(
+  "./os-info.txt",
+  `${arquitetura}\n${plataforma}\n${usuario}\n${versao}`
+);
